cce23336178dbfe6cd7346476331d1c701c78f7b
aryamanghaisas <aryaman0046@gmail.com>
2025-03-24 22:45:14 +0530

    n

--- frontend/src/routes/About.js
+++ frontend/src/routes/About.js
@@ -2,14 +2,14 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import AboutUs from "../components/AboutUs";
-const aboutImage = "/night.jpg";
+
 function About (){
     return(
         <>
         <Navbar />
         <Hero 
         cName="hero-mid"
-        heroImg={aboutImage}
+        heroImg="/assets/night.jpg"
         title="About"
         btnClass="hide"
         />

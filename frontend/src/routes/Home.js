a8b7cd5ca57a9fe96539ece8c9fc684de8a9d3a6
aryamanghaisas <aryaman0046@gmail.com>
2025-03-25 07:22:15 +0530

    m

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -4,7 +4,7 @@ import Hero from "../components/Hero";
 import Destination from "../components/Destination";
 import Trip from "../components/Trip";
 import Footer from "../components/Footer";
-const heroImage = "/12.jpg";
+const heroImage = "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 function Home (){
     return(
         <>

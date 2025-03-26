07be4a1043d7a69981f040eaa0bc0e50ccb650af
aryamanghaisas <aryaman0046@gmail.com>
2025-03-26 22:12:25 +0530

    minor changes

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -4,14 +4,14 @@ import Hero from "../components/Hero";
 import Destination from "../components/Destination";
 import Trip from "../components/Trip";
 import Footer from "../components/Footer";
-const heroImage = "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+//const heroImage = "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 function Home (){
     return(
         <>
         <Navbar />
         <Hero 
         cName="hero"
-        heroImg={heroImage}
+        heroImg="/assets/photo-1498307833015-e7b400441eb8.jpeg"
         title="Your All-In-One Travel Planner"
         text="Choose Your Favourite Destination"
         buttonText="Travel Plan"
